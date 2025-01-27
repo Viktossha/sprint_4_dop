cbb8e2558c7a29edffd7679dab53e77ea66af2a5
Vika <vikapetrova8@gmail.com>
2025-01-27 23:31:18 +0300

    get request - getDecks

--- src/features/decks/DecksList/DecksList.tsx
+++ src/features/decks/DecksList/DecksList.tsx
@@ -1,5 +1,13 @@
 import s from './DecksList.module.css'
+import { useEffect } from 'react'
+import axios from 'axios'
+import { decksApi, instance } from '../decks-api.ts'
 
 export const DecksList = () => {
+
+  useEffect(() => {
+    decksApi.getDecks().then(res => console.log(res.data))
+  }, [])
+
   return <ul className={s.list}></ul>
 }
--- src/features/decks/decks-api.ts
+++ src/features/decks/decks-api.ts
@@ -6,3 +6,35 @@ export const instance = axios.create({
     'x-auth-skip': true,
   },
 })
+
+export const decksApi = {
+  getDecks() {
+    return instance.get<fetchDecks>('/v2/decks')
+  },
+}
+
+type fetchDecks = {
+  items: Array<DeckType>
+  pagination: {
+    currentPage: number
+    itemsPerPage: number
+    totalItems: number
+    totalPages: number
+  }
+}
+
+type DeckType = {
+  isFavorite: boolean
+  author: {
+    id: string
+    name: string
+  },
+  id: string
+  userId: string
+  name: string
+  isPrivate: boolean
+  cover: string
+  created: string
+  update: string
+  cardsCount: number
+}
